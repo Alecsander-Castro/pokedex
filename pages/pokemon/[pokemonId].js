d5f2705cb7c405837e577b07c404503737823f4b
Alecsander Castro <alec_xlive2@hotmail.com>
2022-06-14 19:37:38 -0300

    use router

--- pages/pokemon/[pokemonId].js
+++ pages/pokemon/[pokemonId].js
@@ -1,5 +1,7 @@
 import Image from "next/image";
-import styles from '../../styles/Pokemon.module.css'
+import styles from '../../styles/Pokemon.module.css';
+
+import {useRouter} from 'next/router';
 
 export const getStaticPaths = async () => {
     const maxPokemons = 100
@@ -34,6 +36,13 @@ export const getStaticProps = async (context) => {
 }
 
 export default function Pokemon({pokemon}){
+
+    const router = useRouter()
+
+    if(router.isFallback){
+        return <div>Carregando...</div>
+    }
+
     return(
         <div className={styles.pokemon_container}>
             <h1 className={styles.title}>{pokemon.name}</h1>
